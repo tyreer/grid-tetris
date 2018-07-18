d109cd6a65f676feebde77e228526969e5603821
Robert Tyree <tyreer@gmail.com>
2018-07-18 23:16:01 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Well from "./containers/well/well";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Well />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/well/well.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./well.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div class="box">1</div>
+      </div>
+    );
+  }
+}
+
+export default App;
